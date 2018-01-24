b1b8972545926646e9154b31466e528e19d48f3d
Nikita Galayko <ngalayko@gmail.com>
2018-01-24 11:43:42 +0300

    added cache tests

--- server/cache/cache_test.go
+++ server/cache/cache_test.go
@@ -1,7 +1,10 @@
 package cache
 
 import (
+	"context"
 	"testing"
+
+	. "gopkg.in/check.v1"
 
 	"github.com/ngalayko/url_shortner/server/logger"
 )
@@ -14,5 +17,28 @@ func Benchmark_Store(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		cache.Store("key", "val")
 	}
+}
 
+type TestCacheSuite struct {
+	cache ICache
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+var suite *TestCacheSuite
+
+var _ = Suite(&TestCacheSuite{})
+
+func (s *TestCacheSuite) SetUpSuite(c *C) {
+	s.cache = FromContext(context.Background())
+}
+
+func (s *TestCacheSuite) Test_Load(c *C) {
+	key, value := "key", "value"
+
+	s.cache.Store(key, value)
+
+	v, ok := s.cache.Load(key)
+	c.Assert(ok, Equals, true)
+	c.Assert(v, Equals, value)
 }
